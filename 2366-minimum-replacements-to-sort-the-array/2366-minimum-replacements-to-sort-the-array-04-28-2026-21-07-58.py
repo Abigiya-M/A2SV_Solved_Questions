fd2df07445051ad41d0771c2c7141913ef0df79e
Abigiya Mulugeta <abigiyayeshua@gmail.com>
2026-04-28 21:07:59 +0300

    Time: 47 ms (29.92%), Space: 33.4 MB (47.73%) - LeetHub

---
+++ 2366-minimum-replacements-to-sort-the-array/2366-minimum-replacements-to-sort-the-array-04-28-2026-21-07-58.py
@@ -0,0 +1,10 @@
+class Solution:
+    def minimumReplacement(self, nums: List[int]) -> int:
+        x = nums[-1]
+        res = 0
+        for a in reversed(nums):
+            k = (a + x - 1) // x
+            x = a // k
+            res += k - 1
+        return res
+ 
